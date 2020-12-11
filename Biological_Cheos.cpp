6a7b805669a2fd357b72cb59dedf59366e229993
Sunnatulla Esanov <actinfiber@gmail.com>
2020-12-11 19:53:04 +0500

    Add files via upload

---
+++ Biological_Cheos.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Biological_Cheos.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Biological_Cheos, "Biological_Cheos" );
